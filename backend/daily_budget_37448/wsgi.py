93147a2a6432a267ecc9a38c5faedd345b2698f3
Crowdbotics <team@crowdbotics.com>
2022-11-01 13:48:33 +0000

    Initial commit.

---
+++ backend/daily_budget_37448/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for daily_budget_37448 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daily_budget_37448.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "daily_budget_37448",
+  url: "https://daily_budget_37448.botics.co",
+  api: "https://daily_budget_37448.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
